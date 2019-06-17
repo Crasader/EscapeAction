dee70bda10e533782ad243fa46e1022ec37d1b8e
seokjin8892 <seokjin8892@gmail.com>
2019-06-17 10:20:13 +0900

    20190617_joy_seokjin

--- Proto/Classes/LayerGame.cpp
+++ Proto/Classes/LayerGame.cpp
@@ -3,8 +3,6 @@
 
 USING_NS_CC;
 
-
-
 bool LayerGame::init()
 {
     //////////////////////////////
@@ -16,18 +14,41 @@ bool LayerGame::init()
 
 	win_size = Director::getInstance()->getWinSize();
 
+	//플레이어 생성
 	player = Sprite::create("CloseNormal.png");
 	player->setPosition(Point(win_size.width * 0.5, win_size.height * 0.5));
 	this->addChild(player);
-
+	
+	//키보드 입력 생성
 	auto keyboard_lis = EventListenerKeyboard::create();
 	keyboard_lis->onKeyPressed = CC_CALLBACK_1(LayerGame::onPress, this);
 	keyboard_lis->onKeyReleased = CC_CALLBACK_1(LayerGame::onRelease, this);
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(keyboard_lis, this);
 
-	//this->schedule(schedule_selector(HelloWorld::Check_Player));
+	//버튼 생성
+	Button = Sprite::create("CloseNormal.png");
+	Button->setPosition(Point(win_size.width * 0.05, win_size.height * 0.5));
+	this->addChild(Button);
 
 	//this is Player branch!
+	/*
+	auto joystick1 = Sprite::create("noname.png");
+	joystick1->setPosition(win_size.width * 0.8, win_size.height *0.2);
+	//sprite->setScale(1);
+	joystick1->setOpacity(100);
+	_joystickPad = joystick1;
+	this->addChild(joystick1);
+
+	auto joystick2 = Sprite::create("noname.png");
+	joystick2->setPosition(win_size.width * 0.8, win_size.height * 0.2);
+	//joystick2->setScale(PADSIZE);
+	joystick2->setOpacity(150);
+	_joystick = joystick2;
+	this->addChild(joystick2);
+	
+
+	_startPoint = Point(win_size.width * 0.8, win_size.height * 0.2);
+	_joystickTouched = false;*/
 
     return true;
 }
@@ -105,3 +126,112 @@ void LayerGame::Move_D(float dt)
 	Point temp_pos = Point(player->getPosition().x, player->getPosition().y - 1);
 	player->setPosition(temp_pos);
 }
+void LayerGame::createButton()
+{
+	//auto nomalButton;
+}
+/*
+void LayerGame::initPad()
+{
+}
+
+void LayerGame::create(Layer* layer, Point point)
+{
+}
+
+Point LayerGame::getDPoint()
+{
+	_joystickPoint = _joystick->getPosition();
+	_dPoint.x = (_joystickPoint.x - _startPoint.x) / ((_joystickPad->getContentSize().width / 2));
+	_dPoint.y = (_joystickPoint.y - _startPoint.y) / ((_joystickPad->getContentSize().width / 2));
+	return _dPoint;
+}
+
+float LayerGame::getDLength()
+{
+	_joystickPoint = _joystick->getPosition();
+
+	return _dLength = sqrtf((_startPoint.x - _joystickPoint.x) * (_startPoint.x - _joystickPoint.x) + (_startPoint.y - _joystickPoint.y) * (_startPoint.y - _joystickPoint.y)) / ((_joystickPad->getContentSize().width / 2));
+}
+
+void LayerGame::TouchesBegan(const std::vector<Touch*>& touches, Event* unused_event)
+{
+	std::vector<Touch*>::const_iterator it = touches.begin();
+	Touch* touch;
+	Point tap;
+
+	for (int i = 0; i < touches.size(); i++) {
+		touch = (Touch*)(*it);
+		if (touch) {
+			tap = touch->getLocation();
+
+			if (_joystickPad->getBoundingBox().containsPoint(tap) && _joystickTouched == false) {
+
+				_joystick->setPosition(touch->getLocation());
+				_joystickTouched = true;
+			}
+		}
+		it++;
+	}
+}
+
+void LayerGame::TouchesMoved(const std::vector<Touch*>& touches, Event* unused_event)
+{
+	std::vector<Touch*>::const_iterator it = touches.begin();
+	Touch* touch;
+	Point tap;
+	Point ntap;
+
+	if (_joystickTouched) {
+
+		for (int i = 0; i < touches.size(); i++) {
+
+			touch = (Touch*)(*it);
+			if (touch) {
+				tap = touch->getLocation();
+
+				if (tap.x > _startPoint.x + (_joystickPad->getContentSize().width / 2)  ) {
+					ntap.x = _startPoint.x + (_joystickPad->getContentSize().width / 2)  ;
+				}
+				else if (tap.x < _startPoint.x - (_joystickPad->getContentSize().width / 2) ) {
+					ntap.x = _startPoint.x - (_joystickPad->getContentSize().width / 2) ;
+				}
+				else {
+					ntap.x = tap.x;
+				}
+
+
+
+				if (tap.y > _startPoint.y + (_joystickPad->getContentSize().height / 2) ) {
+					ntap.y = _startPoint.y + (_joystickPad->getContentSize().height / 2) ;
+				}
+				else if (tap.y < _startPoint.y - (_joystickPad->getContentSize().height / 2)) {
+					ntap.y = _startPoint.y - (_joystickPad->getContentSize().height / 2) ;
+				}
+				else {
+					ntap.y = tap.y;
+				}
+
+
+				_joystick->setPosition(ntap);
+
+
+
+			}
+			it++;
+		}
+	}
+}
+
+void LayerGame::TouchesEnded(const std::vector<Touch*>& touches, Event* unused_event)
+{
+	_joystick->setPosition(_startPoint);
+	_joystickTouched = false;
+}
+
+void LayerGame::TouchesCancelled(const std::vector<Touch*>& touches, Event* unused_event)
+{
+	_joystick->setPosition(_startPoint);
+	_joystickTouched = false;
+}
+*/
--- Proto/Classes/LayerGame.h
+++ Proto/Classes/LayerGame.h
@@ -2,6 +2,7 @@
 #define __LAYER_GAME_H__
 
 #include "cocos2d.h"
+#include "cocos-ext.h"
 
 USING_NS_CC;
 
@@ -11,6 +12,7 @@ public:
     virtual bool init();
 
 	Sprite* player;
+	Sprite* Button;
 
 	Size win_size;
 
@@ -21,6 +23,33 @@ public:
 	void Move_L(float dt);
 	void Move_U(float dt);
 	void Move_D(float dt);
+	/*
+	//pad
+	void initPad();
+	void create(Layer* layer, Point point);
+
+	Point _startPoint;
+	Point _joystickPoint;
+	Sprite* _joystick;
+	Sprite* _joystickPad;
+
+	bool _joystickTouched;
+
+	//--------handle direct
+	Point _dPoint;
+	float _dLength;
+
+	Point getDPoint();
+	float getDLength();
+
+	//--------touch events
+	virtual void TouchesBegan(const std::vector<Touch*>& touches, Event* unused_event);
+	virtual void TouchesMoved(const std::vector<Touch*>& touches, Event* unused_event);
+	virtual void TouchesEnded(const std::vector<Touch*>& touches, Event* unused_event);
+	virtual void TouchesCancelled(const std::vector<Touch*>& touches, Event* unused_event);
+	*/
+
+	void createButton();
 
     CREATE_FUNC(LayerGame);
 };
