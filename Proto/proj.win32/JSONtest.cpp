854dc1cff40965069220299baa58082764204580
KimHeeRyeong <khr95039@gmail.com>
2019-07-02 13:10:42 +0900

    맵  json

--- Proto/proj.win32/JSONtest.cpp
+++ Proto/proj.win32/JSONtest.cpp
@@ -2,26 +2,17 @@
 
 bool JSONtest::init()
 {
+	testLevel = 5;
+	rmCnt = (testLevel * 2) + 2;//1레벨 올라갈때마다 방+2, 1Level : 4개, 2Level: 6개...
+	bonusRmCnt = RandomHelper::random_int((int)rmCnt/3, (int)rmCnt/2);//보너스 방
+	parse_resName();//리소스 이름 파일 open, res_name에 
+
+	parse_mapData();
 	setData();
 	return true;
 }
-
-JSONtest::~JSONtest()
+void JSONtest::setData()//fieldData 파일에 setData
 {
-}
-void JSONtest::setData()
-{
-/*	FILE* fp = fopen("fieldData.json", "r");
-	jsonDoc->SetObject();
-	rapidjson::Document::AllocatorType& allocator = jsonDoc->GetAllocator();
-	char readBuffer[500];
-	rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
-	jsonDoc->ParseStream(is);
-	const rapidjson::Value& a = (*jsonDoc)["room"];
-	//log("%s", );
-	//log("%s",(*jsonDoc)["room"].GetString());
-	fclose(fp);*/
-
 	FILE* fp = fopen("fieldData.json", "rb");
 	char readBuffer[500];
 	FileReadStream is(fp, readBuffer, sizeof(readBuffer));
@@ -30,4 +21,45 @@ void JSONtest::setData()
 
 	fclose(fp);
 	
+}
+
+void JSONtest::parse_mapData()
+{
+	
+	map_data = NULL;
+	map_data.SetObject();
+	Document::AllocatorType& a = map_data.GetAllocator();
+	
+	//"info" 설정
+	rapidjson::Value info(kObjectType);
+	info.AddMember("height", rapidjson::Value().SetInt(120), a);
+	info.AddMember("width", rapidjson::Value().SetInt(48), a);
+	map_data.AddMember("info",info, a);
+
+	//"struct" (뼈대)설정
+	rapidjson::Value sct(kArrayType);
+	rapidjson::Value fl(kArrayType);
+	
+
+
+	FILE* fp = fopen("mapDataTest.json", "wb");
+	char wirteBuffer[500];
+	FileWriteStream os(fp, wirteBuffer, sizeof(wirteBuffer));
+	Writer<FileWriteStream> writer(os);
+	map_data.Accept(writer);
+	fclose(fp);
+
+}
+
+
+void JSONtest::parse_resName()
+{
+	FILE* fname = fopen("ResourceName.json", "rb");
+	char readBuffer_fname[500];
+	FileReadStream name_s(fname, readBuffer_fname, sizeof(readBuffer_fname));
+	res_name.ParseStream(name_s);
+	fclose(fname);
+}
+JSONtest::~JSONtest()
+{
 }
